67b94f502d45f7ade689f30f6c71fd8617d41724
webpawan <pawansaini66477@gmail.com>
2023-07-17 15:50:01 +0530

    project all functionality is done

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,14 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./style.css";
 
 import { Link } from "react-router-dom";
-
 const Data = [
   {
     id: "1",
     name: "Man jacket",
-    price: "1,190",
+    price: 1190,
     quantity: 2,
     img: "/images/1.4.webp",
     size: "L",
@@ -16,7 +15,7 @@ const Data = [
   {
     id: "2",
     name: "Track jacket",
-    price: "1,100",
+    price: 1100,
     quantity: 2,
 
     img: "/images/3.0.webp",
@@ -25,15 +24,36 @@ const Data = [
   {
     id: "3",
     name: "Man shirt",
-    price: "1,290",
+    price: 1290,
     quantity: 1,
 
     img: "/images/4.0.webp",
     size: "L",
   },
 ];
+const calculateTotalPrice = (data) => {
+  let total = 0;
+  data.forEach((item) => {
+    total += item.price * item.quantity;
+  });
+  return total;
+};
 
 const Cart = () => {
+  const [cartData, setCartData] = useState(Data);
+  const [totalPrice, setTotalPrice] = useState(calculateTotalPrice(Data));
+  const handleDelete = (index) => {
+    const updatedCartData = cartData.filter((item) => item.id !== index);
+    const updatedTotalPrice = calculateTotalPrice(updatedCartData);
+    setCartData(updatedCartData);
+    setTotalPrice(updatedTotalPrice);
+  };
+
+  useEffect(() => {
+    const updatedTotalPrice = calculateTotalPrice(cartData);
+    setTotalPrice(updatedTotalPrice);
+  }, [cartData]);
+
   return (
     <div className="container-fluid cart">
       <div className=" detail__navigations mt-5 mb-4 d-flex justify-content-between align-item-center">
@@ -43,16 +63,19 @@ const Cart = () => {
         <h1>My Cart </h1>
         <span className="cart__count--container ">
           <img src="/icons/bell.png" alt="" className="img-fluid" />
-          <p className="cart__count">1</p>
+          <p className="cart__count">{cartData.length}</p>
         </span>
       </div>
       <div className="row mx-auto">
         <div className="col-12 col-md-7 mx-auto d-flex flex-column px-2 mb-3">
-          {Data.map((item) => {
+          {cartData.map((item) => {
             return (
-              <div className="cart__container bg__gray d-flex flex-row my-2">
+              <div
+                className="cart__container bg__gray d-flex flex-row my-2"
+                key={item.id}
+              >
                 <div className="cart__product--img  d-flex ">
-                  <img src={item.img} alt="" srcset="" className="img-fluid" />
+                  <img src={item.img} alt="" className="img-fluid" />
                 </div>
                 <div className="cart__product--info w-100  d-flex justify-content-between  ">
                   <div className="d-flex h-100 flex-column justify-content-between  mx-2">
@@ -64,16 +87,19 @@ const Cart = () => {
                   </div>
                   <div className="cart__product--buttons mt-2 ">
                     <div className="d-flex  h-100 flex-column justify-content-between  ">
-                      <div className="d-flex justify-content-end text-danger">
-                        <i class="fa-regular fa-trash-can "></i>
+                      <div
+                        className="d-flex justify-content-end text-danger delete__icon"
+                        onClick={() => handleDelete(item.id)}
+                      >
+                        <i className="fa-regular fa-trash-can "></i>
                       </div>
                       <div className="buttons d-flex align-items-center ">
                         <button className=" ">
-                          <i class="fa-solid fa-minus"></i>
+                          <i className="fa-solid fa-minus"></i>
                         </button>
                         <div className="mx-2 mb-0">{item.quantity}</div>
                         <button className=" ">
-                          <i class="fa-solid fa-plus"></i>
+                          <i className="fa-solid fa-plus"></i>
                         </button>
                       </div>
                     </div>
@@ -95,7 +121,9 @@ const Cart = () => {
             <ul className=" p-0">
               <li className="w-full d-flex  justify-content-between align-items-center  mt-4">
                 <span className="p-0 m-0">sub-total</span>
-                <span>INR 5,870</span>
+                <span>
+                  {totalPrice === 0 ? "INR 0.00" : `INR ${totalPrice}`}
+                </span>
               </li>
               <li className="w-100 d-flex justify-content-between align-items-center my-3">
                 <span>VAT (%)</span>
@@ -103,12 +131,12 @@ const Cart = () => {
               </li>{" "}
               <li className="d-flex justify-content-between align-items-center">
                 <span>shipping-fee</span>
-                <span>INR 80</span>
+                {totalPrice === 0 ? `INR 0,00` : `INR 80`}
               </li>
               <hr />
               <li className="d-flex justify-content-between align-items-center">
                 <span>Total</span>
-                <span>INR 5,870</span>
+                <span>{totalPrice !== 0 ? `INR ${totalPrice + 80}` : 0}</span>
               </li>
             </ul>
           </div>
@@ -117,7 +145,7 @@ const Cart = () => {
             <button className="btn btn-dark w-100 ">
               checkout{" "}
               <span className="mx-2">
-                <i class="fa-solid fa-arrow-right"></i>
+                <i className="fa-solid fa-arrow-right"></i>
               </span>
             </button>
           </div>
